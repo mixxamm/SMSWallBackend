037bd0e562457f7a34395bfdfe05042d4869d374
mixxamm <maximjanssensj5@gmail.com>
2020-08-03 16:50:03 +0200

    Vervang voorbeeldcode

---
+++ SMSWallBackend/Controllers/SMSController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SMSWallBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SMSController : ControllerBase
+    {
+        private readonly ILogger<SMSController> _logger;
+
+        public SMSController(ILogger<SMSController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<SMS> Get()
+        {
+            return new List<SMS> { new SMS() { Date = new DateTime(), Message = "test" } };
+        }
+    }
+}
---
+++ SMSWallBackend/SMS.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SMSWallBackend
+{
+    public class SMS
+    {
+        public DateTime Date { get; set; }
+
+        public string Message { get; set; }
+    }
+}
